06850ab5f36fc600335356f2b0033639ccb8edc8
ProfessorG <>
2016-09-12 22:23:40 -0700

    Exercise 3.2.

--- ComputerScienceProjects/src/computerscienceprojects/ComputerScienceProjects.java
+++ ComputerScienceProjects/src/computerscienceprojects/ComputerScienceProjects.java
@@ -16,6 +16,8 @@ public class ComputerScienceProjects {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println(4.5 % 2);
     }
     
 }
