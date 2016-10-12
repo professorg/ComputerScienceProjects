13a0053ea724360a028614fe7ac0e8f8af514fe0
gvandomelen19 <gvandomelen19@S-RM43-28.jesuitpdx.org>
2016-10-12 15:26:39 -0700

    ASCII

--- ComputerScienceProjects/src/computerscienceprojects/other/misc/ASCIIGradient.java
+++ ComputerScienceProjects/src/computerscienceprojects/other/misc/ASCIIGradient.java
@@ -5,24 +5,28 @@
  */
 package computerscienceprojects.other.misc;
 
+import util.Noise;
+
 /**
  *
  * @author gvandomelen19
  */
 public class ASCIIGradient {
     
-    public static final String CHARS = " .'\"i$%#";
+    public static final String CHARS = " .:;+=&xX$#";
     public static final double NOISE = 0.01;
     
     public static void main(String[] args) {
         
-        double[][] rows = new double[20][60];
+        double[][] rows = new double[40][100];
+        Noise n = new Noise(Math.random());
+        Noise m = new Noise(Math.random());
         
         for (int i = 0; i < rows.length; i++) {
             
             for (int j = 0; j < rows[0].length; j++) {
                 
-                rows[i][j] = clamp(((i + j) / 2.0) / ((rows.length + rows[0].length) / 2) + (Math.random() * NOISE) - 0.5);
+                rows[i][j] = clamp((((n.multiBetterOffset(i, j, 100, 0.05) + 1) + (m.multiBetterOffset(i, j, -100, 0.05) + 1))) / 3.0/* > 2.5 ? 2 : -2*/  + (Math.random() * NOISE) - 0.5);
             }
         }
         
