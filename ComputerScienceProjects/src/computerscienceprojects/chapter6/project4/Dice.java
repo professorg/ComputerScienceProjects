f648a641ae33e33bd85ec5b19a4a26e89342a3f9
ProfessorG <grant@10.0.0.119>
2016-11-29 23:11:27 -0800

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧

--- ComputerScienceProjects/src/computerscienceprojects/chapter6/project4/Dice.java
+++ ComputerScienceProjects/src/computerscienceprojects/chapter6/project4/Dice.java
@@ -36,4 +36,47 @@ public class Dice {
         val = r.nextInt(6) + 1;
         return val;
     }
+    
+    /*
+        public static void main(String[] args) {
+        
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        
+        Scanner reader = new Scanner(System.in);
+        System.out.print("How much money do you have? ");
+        int money = reader.nextInt();
+        int max = money;
+        int rolls = 0;
+        int maxroll = 0;
+        
+        while(money > 0) {
+            
+            rolls++;
+            
+            if(d1.roll() + d2.roll() == 7) {
+                money += 4;
+            } else {
+                money -= 1;
+            }
+            
+            if(money > max) {
+                
+                max = money;
+                maxroll = rolls;
+            }
+        }
+        
+        System.out.println("You ran out of money after " + rolls + " rolls.");
+        System.out.println("You had the most money after " + maxroll
+                + " rolls when you had $" + max);
+    */
+    
+    /*
+    run:
+    How much money do you have? 200
+    You ran out of money after 2160 rolls.
+    You had the most money after 27 rolls when you had $223
+    BUILD SUCCESSFUL (total time: 1 second)
+    */
 }
--- ComputerScienceProjects/src/computerscienceprojects/chapter6/project9/ClickableColorPanel.java
+++ ComputerScienceProjects/src/computerscienceprojects/chapter6/project9/ClickableColorPanel.java
@@ -35,4 +35,31 @@ public class ClickableColorPanel extends JPanel {
         }
     }
     
+    /*
+        public static void main(String[] args) {
+
+            JFrame theGUI = new JFrame();
+            theGUI.setTitle("GUI Program");
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            theGUI.setSize(screenSize);
+            theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Container pane = theGUI.getContentPane();
+
+            int rows = Integer.parseInt(JOptionPane.showInputDialog(theGUI, 
+                    "Enter the number of rows:", JOptionPane.PLAIN_MESSAGE));
+            int cols = Integer.parseInt(JOptionPane.showInputDialog(theGUI, 
+                    "Enter the number of columns:", JOptionPane.PLAIN_MESSAGE));
+
+            pane.setLayout(new GridLayout(cols, rows));
+
+            for (int i = 0; i < rows*cols; i++) {
+
+                pane.add(new ClickableColorPanel(Color.WHITE));
+            }
+
+            theGUI.setLocationRelativeTo(null);
+            theGUI.setVisible(true);
+        }
+    */
+    
 }
